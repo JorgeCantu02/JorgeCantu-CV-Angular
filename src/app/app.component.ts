36e5efad26117f330d217c839cef5bad9e1d359f
JorgeCantu02 <jorge.cramirez02@hotmail.com>
2024-06-22 14:05:37 -0600

    Se agrego el componente de user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, UserComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styles:"",
 })
 export class AppComponent {
-  title = 'angular-cv-app';
+  title = 'propia pagina web';
 }
